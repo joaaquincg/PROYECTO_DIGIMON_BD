ce75f8e8c36638a7d5279efa382fe8bc1cd3a829
M4rK0SDaw <74068144+M4rK0SDaw@users.noreply.github.com>
2022-03-09 17:47:49 +0100

    Actualizacion 1.2
    En esta actualización corrijo el error de un ater table que le faltaba la constraint y su respectivo nombre.

--- DIGIMON.sql
+++ DIGIMON.sql
@@ -1,15 +1,15 @@
 
 -- ____________________________________________________________________
 
-create database DIGIMON_BD;
-use DIGIMON_BD;
+create database DIGIMON_BD2;
+use DIGIMON_BD2;
 
 CREATE TABLE PLAYER (
 NAME_J VARCHAR(20),
 PASSWD VARCHAR(32) NOT NULL,
 MATCH_WIN INT NOT NULL,
 MATCH_PLAY INT NOT NULL,
-constraint JU_PK primary KEY( NAME_J)
+constraint PK_PL primary KEY( NAME_J)
 );
 
 CREATE TABLE DIGIMON(
@@ -20,28 +20,30 @@ ATTACK  INT NOT NULL,
 DEFENSE INT NOT NULL,
 TYPE_DIGI INT NOT NULL,
 LVL INT ,
-constraint DIG_PK primary key (ID_DIGIMON)
+constraint PK_DIGI primary key (ID_DIGIMON)
 );
-alter table DIGIMON FOREIGN KEY (ID_EVOLUTION) references DIGIMON(ID_DIGIMON);
+
+alter table DIGIMON 
+ADD constraint FK_DIGI FOREIGN KEY (ID_EVOLUTION) references DIGIMON(ID_DIGIMON);
+
+
 /*
 He creado el campo campo del id_evolucion para poder asociarlo a su proxima digievolucion,
 ya que se puede definir al crear un digimon nuevo.
 */
 
-CREATE TABLE TENER_DIGI(
+CREATE TABLE HAV_DIGI(
 NAME_J VARCHAR(20),
 NAME_Digi VARCHAR(20),
-Is_In_TEAM bool,
+Is_In_TEAM bool not null ,
 constraint TD_PK PRIMARY KEY ( NAME_J,NAME_Digi)
 );
 
-alter table TENER_DIGI
-add constraint TD_JU_FK foreign key (NAME_J ) references PLAYER (NAME_J );
+alter table HAV_DIGI
+add constraint FK_HV_PL foreign key (NAME_J ) references PLAYER (NAME_J );
 
-alter table TENER_DIGI
-add constraint TD_DI_FK foreign key (NAME_Digi ) references DIGIMON (NAME_Digi );
-
-
+alter table HAV_DIGI
+add constraint FK_HV_DI foreign key (NAME_Digi ) references DIGIMON (NAME_Digi );
 
 
 
